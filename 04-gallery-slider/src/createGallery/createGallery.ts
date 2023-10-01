7c9e97ca0adccbee9414e1757d04862a08adc26c
S1mon009 <105738321+S1mon009@users.noreply.github.com>
2023-10-01 17:20:03 +0200

    Delete 04-gallery-slider directory

--- 04-gallery-slider/src/createGallery/createGallery.ts
+++
@@ -1,8 +0,0 @@
-import { createButtons } from "./createButtons";
-import { createImages } from "./createImages";
-import { images } from "../main";
-export function createGallery() {
-  return `<div class="position-absolute top-50 start-50 translate-middle gallery">${createImages(
-    images
-  )}${createButtons(images.length)}</div>`;
-}
--- 04-gallery-slider/src/createGallery/createImages.ts
+++
@@ -1,5 +0,0 @@
-export function createImages(images: string[]) {
-  return images.map((image) => {
-    return `<img src="/${image}" class="position-absolute start-0 w-100" lazy/>`;
-  });
-}
